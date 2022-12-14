cbdac29f0f3d2437f14bb899f4a3b72f2f38f214
leah wanjiku <leagm22@gmail.com>
2022-12-14 19:37:46 +0300

    added details

---
+++ tagbooks/index.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function(){
+    //getbooks();
+});
+// function getbooks(){
+//     fetch("https://www.googleapis.com/books/v1/volumes?q=javascript&key=AIzaSyAxz_bHZoMfBLyLyEk7PEL9Ygeg3Kw2IyM")
+//     .then((response)=>response.json)
+//     .then(vitabu);
+// }
+// function vitabu(books){
+//     books.forEach(inputdetails);
+// }
+// create a input form for details
+    const dc = document.querySelector("#forms");
+    const form = document.createElement("form");
+    dc.appendChild(form);
+//  creating titleinput
+   const titleInput = document.createElement("input");
+    titleInput.type="text";
+    titleInput.name="title";
+    titleInput.placeholder="enter title";
+    titleInput.id ="formInput";
+    form.appendChild(titleInput);
+//  creating author input
+    const authorInput = document.createElement("input");
+    authorInput.type="text";
+    authorInput.name = "author";
+    authorInput.placeholder="enter author";
+    authorInput.id="formInput";
+    form.appendChild(authorInput);
+    document.body.appendChild(form);
+//create a add me button
+const btn =document.createElement("button");
+button.id ="btns"
+
+
+
