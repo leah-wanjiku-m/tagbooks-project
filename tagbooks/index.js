d58f88c92a0be27d7605d2a704b9f297be7bd492
leah wanjiku <leagm22@gmail.com>
2022-12-15 10:59:32 +0300

    add details

--- tagbooks/index.js
+++ tagbooks/index.js
@@ -1,12 +1,13 @@
 document.addEventListener("DOMContentLoaded", function(){
     getbooks();
 });
- function getbooks(){fetch("https://www.googleapis.com/books/v1/volumes?q={title}&key=AIzaSyAxz_bHZoMfBLyLyEk7PEL9Ygeg3Kw2IyM")
-     .then((response)=>response.json)
-     .then(vitabu);
+ function getbooks(){fetch("https://www.googleapis.com/books/v1/volumes?q={javascript}&key=AIzaSyAxz_bHZoMfBLyLyEk7PEL9Ygeg3Kw2IyM")
+     .then((response)=> response.json())
+     .then(vitabu); 
+   
      }
-// function vitabu(books){
-//     books.forEach(inputdetails);
+ //function vitabu(books){
+   //  books.forEach(inputdetails);
 // }
 // create a input form for details
     const dc = document.querySelector("#forms");
