fb021eda02822a43495132bfeec08cfeed8dc8bf
leah wanjiku <leagm22@gmail.com>
2022-12-16 08:09:48 +0300

    added  a detailed readme

---
+++ tagbooks/README.md
@@ -0,0 +1,18 @@
+# TAGBOOKS
+
+## INTRODUCTION
+This project is a booklist webapplication  that allow users to view book covers add to their favourites and delete from favourites.
+## DESCRIPTION
+its an app that helps to add and delete book from favourites.
+
+## FUNCTIONALITY
+To use tagbooks you'll be able to view the book covers on books related to javascript.
+Enter the name of the book in the title section then press the submit button that will add book to list 
+to delete book press the remove buuton to remove fro list.
+## CREDIT
+Acknowledgement I thank google books api for providing me acess to their Api 
+## LICENCE
+
+Copyright (c) [2019] [DuncanLangat6515]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: R The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
--- tagbooks/index.js
+++ tagbooks/index.js
@@ -1,32 +1,76 @@
 document.addEventListener("DOMContentLoaded", function(){
     getbooks();
 });
+function appendElement(element,id="dec"){
+  const rootElement= document.getElementById(id);
+  rootElement.append(element);
+};
  function getbooks(){fetch("https://www.googleapis.com/books/v1/volumes?q={javascript}&key=AIzaSyAxz_bHZoMfBLyLyEk7PEL9Ygeg3Kw2IyM")
      .then((response)=> response.json())
-     .then(vitabu); 
+     .then((data) => {
+      for(let i=0;i<5;i++){
+        let books= data.items[i];
+        let thumbs=books.volumeInfo;
+        let thumb= thumbs.imageLinks;
+        console.log(books)
+        let dr = createMenuDetails (books.kind,books.id, thumbs.title,thumb.thumbnail);
+        appendElement(dr,"menubar")  
+      }
+     }
+     )
    
      }
      //creating a menu where user can view books
-     function createMenuDetails(item){
-        const menuBar= document.querySelector("#menubar");
-        const ul = document.createElement("ul");
-        const li= document.createElement("li");
-        ul.innerHTML =item.title;
-        ul.appendChild("li");
-        menuBar.appendChild("ul")
-        li.addEventListener("click",() => {
-          details(item);
-        });
+     function createMenuDetails(kind,id,title,thumbnail){
+        //const menuBar= document.querySelector("#menubar");
+        const ul = document.createElement("div");
+        const li= document.createElement("div");
+        const bookind= document.createElement("p");
+        const bookVolume=document.createElement("p");
+        // image element
+        const bookImage=document.createElement("img");
+        bookImage.src = (thumbnail);
+
+
+        bookind.innerHTML=(kind);
+         const bookId= document.createElement("p");
+         bookId.innerHTML=(id);
+         bookId.style.color=("pink");
+         bookVolume.innerHTML=(title);
+         bookImage.src = (thumbnail);
+        ul.appendChild(li);
+        li.append(bookind);
+        li.append(bookId);
+        li.append(bookVolume);
+        li.append(bookImage);
+        return li
      }
-     //details of the sidebar
-     function details(item){
-      const cover=document.querySelector("#cover");
-      const title= document.querySelector("#title");
-      const author=document.querySelector("#author");
-      cover.src=item.thumbnail;
-      title.innerHTML =item.title;
-      author.innerHTML =item.authors;
+     
+     //creating  a form and adding an event listener
+     function event(){
+      let form =document.querySelector('bookform');
+      form.addEventListener("submit", (e) =>{
+        bookAdd(e.target.addFavourites.value);
+        form.reset()
+      })
      }
+     function bookAdd(){
+      let p=document.createElement("p");
+      let btn=document.createElement("button");
+      btn.addEventListener("click", handleDelete);
+      btn.innerHTML='remove';
+      p.innerHTML= ` ${ addFavourites} `
+      p.appendChild(btn);
+      console.log(p);
+      document.querySelector('bookcontainer').appendChild(p);
+     }
+     function handleDelete(e){
+      e.target.parentNode.remove()
+     }
+
+
+    
+
 //  //function vitabu(books){
 //    //  books.forEach(inputdetails);
 // // }
