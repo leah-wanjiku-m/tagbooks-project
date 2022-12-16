c82cf9db5d0e0af15691433bf674dc0d65cff046
leah wanjiku <leagm22@gmail.com>
2022-12-16 11:50:27 +0300

    added details

--- tagbooks/index.js
+++ tagbooks/index.js
@@ -5,7 +5,8 @@ function appendElement(element,id="dec"){
   const rootElement= document.getElementById(id);
   rootElement.append(element);
 };
- function getbooks(){fetch("https://www.googleapis.com/books/v1/volumes?q={javascript}&key=AIzaSyAxz_bHZoMfBLyLyEk7PEL9Ygeg3Kw2IyM")
+ function getbooks(){
+  fetch(`https://www.googleapis.com/books/v1/volumes?q={javascript}&key=`)
      .then((response)=> response.json())
      .then((data) => {
       for(let i=0;i<5;i++){
@@ -70,45 +71,6 @@ function appendElement(element,id="dec"){
 
 
     
-
-//  //function vitabu(books){
-//    //  books.forEach(inputdetails);
-// // }
-// // create a input form for details
-//     const dc = document.querySelector("#forms");
-//     const form = document.createElement("form");
-//     dc.appendChild(form);
-// //  creating titleinput
-//    const titleInput = document.createElement("input");
-//     titleInput.type="text";
-//     titleInput.name="title";
-//     titleInput.placeholder="enter title";
-//     titleInput.id ="formInput";
-//     form.appendChild(titleInput);
-// //  creating author input
-//     const authorInput = document.createElement("input");
-//     authorInput.type="text";
-//     authorInput.name = "author";
-//     authorInput.placeholder="enter author";
-//     authorInput.id="input";
-//     form.appendChild(authorInput);
-//     document.body.appendChild(form);
-//     form.addEventListener("submit",(e) =>{
-//         e.preventDefault();
-//     });
-// //create a add me button
-// const but =document.querySelector("#forms");
-// const btn =document.createElement("button");
-// btn.id ="btns"
-// btn.innerHTML ="ADD BOOK";
-// but.appendChild(btn);
-// //add an event listener
-// //btn.addEventListener("click",vitabu);
-// //function vitabu(){
-//  //const bookTitle = document.querySelector("#formInput")
-//  //bookTitle.innerHTML = vitabu.title;  
-// //}
-// c
 
 
 
